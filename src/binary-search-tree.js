cd27322e73413a07e094b7e548238aaf9a4402c2
Mikhail <tyuris@icloud.com>
2025-10-27 16:20:12 +0300

    feat: remove function

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -79,7 +79,37 @@ class BinarySearchTree {
 
   remove(data) {
     // Remove line below and write your code here
-    return data;
+    function removeNode(node, data) {
+      if (node === null) {
+        return null;
+      }
+      if (data === node.data) {
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+        if (node.left === null) {
+          return node.right;
+        }
+        if (node.right === null) {
+          return node.left;
+        }
+
+        let tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    }
+    this.node = removeNode(this.node, data);
   }
 
   min() {
